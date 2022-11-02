c29c0663c0b756f21b7fcb4d07058b3f84cea640
Sxprite 09 <velascofranki445@gmail.com>
2022-11-02 15:18:43 -0500

    se añadio el DailyMeet y se creo el modulo

---
+++ Modules/PTODEVENTA/Database/Seeders/PTODEVENTADatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\PTODEVENTA\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class PTODEVENTADatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
---
+++ Modules/PTODEVENTA/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('ptodeventa::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('ptodeventa.name') !!}
+    </p>
+@endsection
